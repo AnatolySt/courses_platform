1506ea079cc23845d3d02732ff0845ada18908c2
Анатолий Ступин <anatolystupin@MacBook-Pro-Anatolij.local>
2021-04-12 22:45:39 +0300

    Add: course model

---
+++ app/models/course.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id          :integer          not null, primary key
+#  title       :string           not null
+#  description :string           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+class Course < ApplicationRecord
+  validates :title, :description, presence: true
+  validates :title, uniqueness: true
+end
